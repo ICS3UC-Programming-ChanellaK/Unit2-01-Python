371b051f2ead945457ae1574019f66ddf82b9116
Chanella Keza <chanella.keza@stu.ocsb.ca>
2025-03-04 18:09:01 +0000

    Added area_per_rect_input.py

---
+++ area_per_rect_input.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created By: Chanella Keza
+# Date: March 2, 2025
+# This program calculate the area and perimeter of 4cm x 3cm of a rectangle
+
+
+def main():
+    print("for a rectangle with length 4cm and width 3cm:")
+    print()
+    print("The area is: {}cm^2".format(4 * 3))
+    print("The perimeter is: {}cm".format(4 + 3))
+
+
+if __name__ == "__main__":
+    main()
